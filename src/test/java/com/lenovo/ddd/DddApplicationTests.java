804a4ba19750a2cef907c0d8660c0d48e5582db2
zhaodj5 <zhaodj5@lenovo.com>
2019-06-17 18:41:59 +0800

    add test case file

--- src/test/java/com/lenovo/ddd/DddApplicationTests.java
+++ src/test/java/com/lenovo/ddd/DddApplicationTests.java
@@ -6,9 +6,7 @@ import com.lenovo.ddd.dto.Ticket;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class DddApplicationTests {
---
+++ src/test/java/com/lenovo/ddd/ParkingLotFactoryTest.java
@@ -0,0 +1,16 @@
+package com.lenovo.ddd;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(MockitoJUnitRunner.class)
+public class ParkingLotFactoryTest {
+
+    @Test
+    public void should_I_not_know_how_do_it_now() {
+        assertTrue(true);
+    }
+}
