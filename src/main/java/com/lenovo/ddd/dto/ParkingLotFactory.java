692486a7191eaa42502a167c9392ee2609a139be
zhaodj5 <zhaodj5@lenovo.com>
2019-06-17 18:38:51 +0800

    update junit

---
+++ src/main/java/com/lenovo/ddd/dto/ParkingLotFactory.java
@@ -0,0 +1,13 @@
+package com.lenovo.ddd.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+public class ParkingLotFactory {
+
+    List<ParkingLot> parkingLots;
+}
--- src/test/java/com/lenovo/ddd/DddApplicationTests.java
+++ src/test/java/com/lenovo/ddd/DddApplicationTests.java
@@ -6,11 +6,11 @@ import com.lenovo.ddd.dto.Ticket;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
 public class DddApplicationTests {
 
     @Test
